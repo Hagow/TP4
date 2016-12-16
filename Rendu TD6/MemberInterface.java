4c69200dc1c6e31108ed7c7d32caaef7da3f8b3f
CHRISTOPHER SABOYA <christopher.saboya@live.fr>
2016-12-16 23:20:39 +0100

    ajout 2 nouvelles classes

---
+++ Rendu TD6/MemberInterface.java
@@ -0,0 +1,10 @@
+package fr.unice.iut.info.reseauSocial.core;
+
+public interface MemberInterface
+{
+    public int getAge ();
+    
+    public String getDescription ();
+    
+    public String ident ();
+}
